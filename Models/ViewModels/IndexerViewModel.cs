83c941707ea7d7bffc7e0d4bbdccf06e50060ef1
Panayot Ivanov <panayot_zhi@abv.bg>
2020-10-06 17:45:31 +0300

    Add model and views.

---
+++ Models/ViewModels/IndexerViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using stranitza.Utility;
+
+namespace stranitza.Models.ViewModels
+{
+    public class IndexerViewModel
+    {
+        public int IssueId { get; set; }
+        
+        public int ReleaseNumber { get; set; }
+        
+        public int ReleaseYear { get; set; }
+        
+        public int IssueNumber { get; set; }
+
+        public StranitzaIndexer.IndexingResult Result { get; set; }
+
+        public ICollection<SourceDetailsViewModel> ExistingSources { get; set; }
+
+        public SelectList Categories { get; set; }
+    }
+}
