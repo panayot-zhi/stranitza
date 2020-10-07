70f428f3d7a26ac7972ad080905adf3d083c4d86
Panayot Ivanov <panayot_zhi@abv.bg>
2020-10-07 13:59:37 +0300

    Add js number formatter.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -45,6 +45,26 @@ function getFriendlySizeName(size) {
     return length.toPrecision(4) + " " + sizeLabelsArray[order];
 }
 
+function nFormatter(num, digits) {
+
+    var si = [
+        { value: 1, symbol: "" },
+        { value: 1E3, symbol: "k" },
+        { value: 1E6, symbol: "M" }
+    ];
+
+    var rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+
+    var i;
+    for (i = si.length - 1; i > 0; i--) {
+        if (num >= si[i].value) {
+            break;
+        }
+    }
+
+    return (num / si[i].value).toFixed(digits).replace(rx, "$1") + si[i].symbol;
+}
+
 function doSubmit(e, target) {
     e && e.preventDefault();
 
